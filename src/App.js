013db8645c26c6d0e0a0a99909a7bc503c148c25
Hamid Kamyab <mr_kamyab@yahoo.com>
2024-04-21 21:50:40 +0330

    Making an accordion component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Question from "./Question";
+import data from "./data";
 
 function App() {
+  const [questions,setQuestions] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex vh-100 vw-100 justify-content-center align-items-center">
+      <div className="questionBox container bg-white d-flex flex-column gap-4 py-4 px-3">
+        <h5 className="mt-2 text-muted">آیا سوالی برایتان پیش آمده؟</h5>
+        <div className="info d-flex flex-column gap-3">
+          {
+            questions.map((question,index)=>{
+              return <Question {...question} key={index} />
+            })
+          }
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,33 @@
+﻿import React,{useState} from 'react';
+import * as AI from "react-icons/ai";
+
+function Question(question) {
+    const [isShow,setIsShow] = useState(false);
+    return (
+        <div id={'q-'+question.id} className='question p-3 rounded-2' onClick={()=>setIsShow(!isShow)} role='button'>
+            <div className='header d-flex aling-items-center justify-content-between'>
+                <h6 className='m-0'>
+                    {question.title}
+                </h6>
+                <button className='border-0 bg-dark text-white rounded-circle d-flex align-items-center justify-content-center' >
+                    {
+                        isShow ?
+                        <AI.AiOutlineMinus size={14} />
+                        :
+                        <AI.AiOutlinePlus size={14} />
+                    }
+                </button>
+            </div>
+
+            {
+                isShow &&
+                <p className='body p-0 text-muted m-0 mt-3'>
+                    {question.body}
+                </p>
+            }
+            
+        </div>
+    )
+}
+
+export default Question
